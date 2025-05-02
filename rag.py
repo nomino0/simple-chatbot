934f769212ba0eb2aefd7a421633add5394658d3
Amine Mayoufi <amine.mayoufi11@gmail.com>
2025-05-02 11:51:19 +0100

    fixes

--- rag.py
+++ rag.py
@@ -1,2 +1,19 @@
 import os
 from dotenv import load_dotenv
+from langchain.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+def load_documents(file_path):
+    if file_path.endswith('.pdf'):
+        loader = PyPDFLoader(file_path)
+    return loader.load()
+
+# chunking the documents
+def split_documents(documents):
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=500,
+        chunk_overlap=30
+    )
+    return splitter.split_documents(documents)
+
+   
